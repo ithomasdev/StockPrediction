a4fc1ce54de548fbe282ac29e77f8d901c313ebf
Richard Wei <rwei@uncc.edu>
2021-07-28 17:42:39 -0400

    create_excel finished

--- data/.ipynb_checkpoints/ticker-checkpoint.py
+++ data/.ipynb_checkpoints/ticker-checkpoint.py
@@ -3,6 +3,7 @@ import pandas as pd
 import dask.dataframe as dd
 from pandas_datareader import data as pdr
 from dask.dataframe import from_pandas
+from os.path import expanduser
 
 # get dataframe using folder and stock name
 # takes csv file and reads + returns the dataframe
@@ -17,10 +18,20 @@ def create_excel(symbol, timeStart, timeEnd):
     #get dataframe for pandas
     pd = pdr.get_data_yahoo(symbol, start=timeStart, end=timeEnd)
     
+    print (pd)
+    
     #convert from pandas to dask dataframe
     df = dd.from_pandas(pd, npartitions=3)
     
+    #make filepath
+    home = expanduser("~")
+    
+    filename = "\Desktop\\" + symbol
+    
+    filepath = home + filename + ".csv"
+    
+    print ("Created in: " + filepath)
+    
     #convert to csv file
-    df.to_csv('/path/to/data/' + symbol + '-*.csv')  
+    df.to_csv(filepath, single_file = True)
     return
-
