b0c633ac298257f138da6d31cd3c9424961caeb4
ithomasdev <ithomasdev@gmail.com>
2021-07-29 19:20:57 -0400

    Added initial structure to Stock class

---
+++ models/Stock.py
@@ -0,0 +1,12 @@
+class Stock:
+    def __init__(self, name, symbol, data):
+        self.name = name
+        self.symbol = symbol
+        self.data = data
+
+    # Sample methods, these would use the dataframe(data) to get values for specific dates     
+    def get_open(date):
+        return
+
+    def get_close(date):
+        return
