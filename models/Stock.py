0afecc884b7c6240b8dd29e80f1cd89257d695a4
KBrisson16 <KyleBrisson16@gmail.com>
2021-08-02 19:11:14 -0400

    Update Stock.py
    Added gets for open,high,low,close methods

--- models/Stock.py
+++ models/Stock.py
@@ -5,8 +5,18 @@ class Stock:
         self.data = data
 
     # Sample methods, these would use the dataframe(data) to get values for specific dates     
-    def get_open(date):
-        return
+    def get_open(self, date, symbol):
+        open_price = symbol.loc[date].open
+        return open_price
 
-    def get_close(date):
-        return
+    def get_close(self, date, symbol):
+        close_price = symbol.loc[date].close
+        return close_price
+
+    def get_high(self, date, symbol):
+        high_price = symbol.loc[date].high
+        return high_price
+
+    def get_low(self, date, symbol):
+        low_price = symbol.loc[date].low
+        return low_price
